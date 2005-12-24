1001fbc054aae8318203c640b9fa29d17d0f5305
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2005-12-24 15:31:01 +0000

    some boilerplate code added
    git-svn-id: file:///svn/rdup/trunk@12 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- rdump.c
+++ rdump.c
@@ -9,10 +9,10 @@
  */
 
 /* cmd options */
-int opt_null;
-int opt_onefilesystem;
-int opt_nobackup;
-int opt_verbose;
+int opt_null = 0;
+int opt_onefilesystem = 0;
+int opt_nobackup = 0;
+int opt_verbose = 0;
 
 int dumptype;
 time_t list_mtime;
@@ -25,6 +25,24 @@ void gfunc_backup(gpointer data, gpointer usr);
 void gfunc_remove(gpointer data, gpointer usr);
 gint gfunc_str_equal(gconstpointer a, gconstpointer b);
 
+
+void
+usage(FILE *f, char *p) 
+{	
+	fprintf(f, "Usage: %s [OPTION...] FILELIST DIR...\n", p);
+	fprintf(f, "%s generates a full or incremental file list, this\n", p);
+	fprintf(f, "list can be used to implement a incremental backup scheme\n");
+	fprintf(f, "\n   FILELIST\tincremental file list\n");
+	fprintf(f, "   \t\tif not found or empty, a full dump is done\n");
+	fprintf(f, "   DIR\t\tdirectory or directories to dump\n");
+	fprintf(f, "\nOptions:\n");
+	fprintf(f, "   -h\t\tgives this help\n");
+	fprintf(f, "   -n\t\tdo not look at" NOBACKUP "files\n");
+	fprintf(f, "   -v\t\tbe more verbose\n");
+	fprintf(f, "   -x\t\tstay in local file system\n");
+	fprintf(f, "   -0\t\tdelimit all output with NULLs\n");
+	fprintf(f, "\nReport bugs to <miek@miek.nl>\n");
+}
 
 /**
  * subtrace list *b from list *a, leaving
@@ -85,29 +103,62 @@ main(int argc, __attribute__((unused)) char **argv)
 	GSList 	*remove;	/* what needs to be rm'd */
 	GSList 	*curlist; 	/* previous backup list */
 	FILE 	*fplist;
-	char 	*dirpath;
+	char 	*progname;
+	gint    i;
+	int 	c;
 
 	curlist = NULL;
 	backup = NULL;
 	remove = NULL;
-	dirpath = NULL;
+	progname = g_strdup(argv[0]);
+	opterr = 0;
 
-	dumptype = INC_DUMP;
-
-	list_mtime = mtime("FILELIST");
+	while ((c = getopt (argc, argv, "hnvx0")) != -1) {
+		switch (c)
+		{
+			case 'h':
+				usage(stdout, progname);
+				exit(EXIT_SUCCESS);
+			case 'n':
+				opt_nobackup = 1;
+				break;
+			case 'v':
+				opt_verbose = 1;
+				break;
+			case 'x':
+				opt_onefilesystem = 1;
+				break;
+			case '0':
+				opt_null = 1;
+				break;
+		}
+	}
+	argc -= optind;
+	argv += optind;
 
 	if (argc < 2) {
-		dirpath = "bin";
+		usage(stdout, progname);
+		exit(EXIT_FAILURE);
 	}
-	
-	if (!(fplist = fopen("FILELIST", "a+"))) {
+
+	/* Check for full of incremental dump */
+	if ((list_mtime = mtime(argv[0])) == 0) {
+		dumptype = NULL_DUMP;
+	} else {
+		dumptype = INC_DUMP;
+	}
+
+	if (!(fplist = fopen(argv[0], "a+"))) {
 		fprintf(stderr, "Could not open file\n");
 		exit(EXIT_FAILURE);
 	} else {
 		rewind(fplist);
 	}
 	curlist = g_slist_read_file(fplist);
-	backup = dir_crawl(dirpath);
+	for (i = 1; i < argc; i++) {
+		backup = g_slist_concat(backup, 
+				dir_crawl(argv[i]));
+	}
 
 	remove = g_slist_substract(curlist, backup); 
 
