dab11876e6ae5eab78ff70faae9fd9b4371c5043
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2005-12-24 09:29:04 +0000

    moved to C
    git-svn-id: file:///svn/rdup/trunk@7 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- rdump.sh
+++ rdump.sh
@@ -1,35 +1,52 @@
 #!/bin/bash
 
-
 # 1. create a dir for the backup
 # 2. link null to dir: null -> dir
 # 3. incremental dumps, dump to null
 # 4. a new null dump removes the link and
 #    goes to step 1
 
-# a bash prototype of my new rdup utility
-# TODO
-# remote stuf
-# encryption - get that in rsync
+case $1 in
+        elektron-etc)
+                DIR="/etc"
+                HOST=$1
+        ;;
+        elektron-homes)
+                DIR="/home"
+                HOST=$1
+        ;;
+        elektron-var-mail)
+                DIR="/var/mail"
+                HOST=$1
+        ;;
+        elektron-var-svn)
+                DIR="/var/svn"
+                HOST=$1
+        ;;
+        elektron-var-lib)
+                DIR="/var/lib"
+                HOST=$1
+        ;;
+        *)
+                echo "Need a host-def as argument"
+                exit 1
+        ;;
+esac
 
 ARCHIVEDIR=/vol/backup
-HOST=elektron-etc
-DIR="/etc"
-
 EXCLUDE="--exclude .svn/ --exclude tmp/ --exclude .slide_img --exclude
 .thumb_img --exclude lost+found --exclude .Cache/"
-
-
 SUFFIX=`date +%Y%m%d%H%M`
 NULLDIR=`date +%Y%m`
-#OPT="-av --stats --compress --delete -b"
-OPT="-av -W --compress --delete -b -S"
+OPT="-a --stats --compress --delete -b"
 # .nobackup files are interpreted by rsync
 FILTER=.nobackup
 
-case $1 in
+case $2 in
         null)
                 mkdir -p $ARCHIVEDIR/$HOST/$NULLDIR
+                chmod 755 $ARCHIVEDIR/$HOST # set liberal permissions
+                chmod 755 $ARCHIVEDIR/$HOST/$NULLDIR # set liberal permissions
                 # remove prev. null link, and add the new one
                 ( cd $ARCHIVEDIR/$HOST; rm -f null )
                 ( cd $ARCHIVEDIR/$HOST; ln -sf $NULLDIR null )
@@ -45,7 +62,10 @@ case $1 in
         ;;
         *)
                 echo "Need: inc or null as argument"
+                exit 1
        ;;
 esac
-        
 
+echo "Succesfully backed up: $HOST ($2)"
+        
+exit 0
