2ec1df47de4d582b530bbc58754eb60501db87a1
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2005-12-24 10:55:52 +0000

    stuff begins to work
    git-svn-id: file:///svn/rdup/trunk@9 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- rdump.c
+++ rdump.c
@@ -1,12 +1,69 @@
 #include "rdump.h"
 
 
-int main(int argc, char **argv) 
-{
-	
-	dir_crawl(stdout, "tmp");
+GSList * dir_crawl(char *path);
 
-	
-	printf("Hallo, ik ben Miek Gieben");
-	return 0;
+
+void 
+print(gpointer *data)
+{
+	fprintf(stdout, "%s\n", (char*) data);
 }
+
+
+/**
+ * subtrace list *b from list *a, leaving
+ * the elements that are only in *a. Essentially
+ * a double diff: A diff (A diff B)
+ */
+#if 0
+GSList *
+g_slist_substract(GSList *a, GSList *b)
+{
+	GSList *diff;
+
+	diff = NULL;
+
+	return diff;
+}
+#endif
+
+GSList *
+g_slist_read_file(FILE *fp)
+{
+	char buf[BUFSIZE + 1];
+	GSList *list;
+
+	list = NULL;
+	while (!(fgets(buf, BUFSIZE, fp))) {
+		list = g_slist_prepend(list,
+				(gpointer*) g_strdup(buf));
+	}
+	return list;
+}
+
+
+int 
+main(int argc, char **argv) 
+{
+	GSList *diskfiles; /* on disk stuff */
+	GSList *listfiles; /* previous backup list */
+	FILE *fplist;
+
+	diskfiles = NULL;
+	listfiles = NULL;
+	
+	if (!(fplist = fopen("FILELIST", "rw"))) {
+		fprintf(stderr, "Could not open file\n");
+	} else {
+		listfiles = g_slist_read_file(fplist);
+	}
+	
+	diskfiles = dir_crawl(argv[1]);
+
+	g_slist_foreach(diskfiles, print, NULL);
+	
+	printf("\n");
+	exit(EXIT_SUCCESS);
+}
+
