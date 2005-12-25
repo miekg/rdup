4ede437e1d1f60d990532d731e301cf15db8f93f
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2005-12-25 18:17:37 +0000

    implemented yesterday partly
    git-svn-id: file:///svn/rdup/trunk@63 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- yesterday.sh
+++ yesterday.sh
@@ -1,13 +1,45 @@
 #!/bin/bash
 
 # inspired by yesterday of plan9
-
-$1=backupdir
-
-# difficulty is how to find the stuff
-
 # -n days ago
 # -c copy
 # -C copy carefull
 # -d diff -u
+backupdir=$1
+bsuffix=`date +%Y%m`
+if [ -z $backupdir ]; then
+        echo "** Need archive directory"
+        exit 1
+fi
+backupdir=$backupdir/$bsuffix
 
+while shift
+do
+        file=$1
+        if [[ -z $file ]]; then
+                continue
+        fi
+        if [[ ${file:0:1} != "/" ]]; then
+                file=`pwd`/$file
+        fi
+        [ ! -e $backupdir$file ] && \
+                echo "** Not found in archive: $file" && continue
+
+        # print
+        echo $backupdir$file
+done
+
+#        # copy
+#        echo cp -a $backupdir$file .
+#        # copy careful
+#        cmp $backupdir$file $file > /dev/null
+#        if [[ $? ]]; then
+#                # copy only when the differ
+#                echo cp -a $backupdir$file .
+#        fi
+#
+#        # diff, only for files and links
+#        echo diff -u `basename $backupdir$file` `basename $file`
+#        [ -f $backupdir$file ] && diff -u $backupdir$file $file
+#        [ -h $backupdir$file ] && diff -u $backupdir$file $file
+#        exit 0
