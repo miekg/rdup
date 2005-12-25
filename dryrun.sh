a545a28228d86ae0b50cbae6b64ada4123561cc5
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2005-12-25 16:57:59 +0000

    added dryrun script
    git-svn-id: file:///svn/rdup/trunk@61 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- dryrun.sh
+++ dryrun.sh
@@ -3,9 +3,9 @@
 # Copyright (c) 2005, 2006 Miek Gieben
 # See LICENSE for the license
 #
-# This actually creates the backup
-# zbackup.sh /target
-# in /target/YYYYMM/ a gzipped mirror is created
+# echo what you want to do to stdout
+# backup.sh /target
+# in /target/YYYYMM/ a mirror is created
 S_ISDIR=16384   # octal: 040000 (This seems to be portable...)
 S_ISLNK=40960   # octal: 0120000
 S_MMASK=4095    # octal: 00007777, mask to get permissions
@@ -16,10 +16,10 @@ if [ -z $backupdir ]; then
         echo "** Need archive directory"
         exit 1
 fi
-mkdir -p $backupdir; mkdir -p $backupdir/$bsuffix
-chown root:backup $backupdir; chown root:backup $backupdir/$bsuffix
-chmod 755 $backupdir; chmod 755 $backupdir/$bsuffix
-backupdir=$backupdir/$bsuffix
+#mkdir -p $backupdir; mkdir -p $backupdir/$bsuffix
+#chown root:backup $backupdir; chown root:backup $backupdir/$bsuffix
+#chmod 755 $backupdir; chmod 755 $backupdir/$bsuffix
+#backupdir=$backupdir/$bsuffix
 
 while read mode uid gid path
 do
@@ -39,21 +39,19 @@ do
                 # add
                 case $typ in
                         0)      # reg file
-                        [ -f $backupdir/$path ] && mv $backupdir/$path $backupdir/$path.$suffix
-                        cat $path | gzip -c > $backupdir/$path
+                        echo "cat $path > $backupdir/$path"
                         ;;
                         1)      # directory
-                        [ ! -d $backupdir/$path ] && mkdir -p $backupdir/$path
+                        echo mkdir -p $backupdir/$path
                         ;;
                         2)      # link
-                        [ -L $backupdir/$path ] && mv $backupdir/$path $backupdir/$path.$suffix
-                        cp -a $path $backupdir/$path
+                        echo cp -a $path $backupdir/$path
                         ;;
                 esac
-                chown $uid:$gid $backupdir/$path
-                chmod $bits $backupdir/$path
+                echo chown $uid:$gid $backupdir/$path
+                echo chmod $bits $backupdir/$path
         else
                 # remove
-                mv $backupdir/$path $backupdir/$path.$suffix
+                echo mv $backupdir/$path $backupdir/$path.$suffix
         fi
 done 
