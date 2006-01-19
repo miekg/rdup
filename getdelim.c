df0709629cc7f257a9c924ad62c9b74b1a8b2c09
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-01-19 19:30:45 +0000

    impl. for getdelim
    git-svn-id: file:///svn/rdup/trunk@219 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- getdelim.c
+++ getdelim.c
@@ -2,18 +2,63 @@
 
 #include "rdup.h"
 
-/* copy glibc source and modified to make it compile */
-ssize_t 
-getdelim(char **lineptr, size_t *n, int delim, FILE *fp) 
-{
-  int result;
-  ssize_t cur_len = 0;
-  ssize_t len;
+#define GETDELIM_BUFFER 128
 
-  if (lineptr == NULL || n == NULL) {
-	  return -1;
-  }
-  return 0;
+/* copied from xine-devel */
+ssize_t
+getdelim( char **lineptr, size_t *n, int delimiter, FILE *stream )
+{
+        char *p;
+        int c;
+        size_t len = 0;
+
+        if (!lineptr || !n || (!*lineptr && *n))
+                return -1;
+
+        /* allocate initial buffer */
+        if (!*lineptr || !*n) {
+                char *np;
+                np = realloc( *lineptr, GETDELIM_BUFFER );
+                if (!np)
+                        return -1;
+                *n = GETDELIM_BUFFER;
+                *lineptr = np;
+        }
+
+        p = *lineptr;
+
+        /* read characters from stream */
+        while ((c = fgetc( stream )) != EOF) {
+                if (len >= *n) {
+                        char *np = realloc( *lineptr, *n * 2 );
+                        if (!np)
+                                return -1;
+                        p = np + (p - *lineptr);
+                        *lineptr = np;
+                        *n *= 2;
+                }
+                *p++ = (char) c;
+                len++;
+                if (delimiter == c)
+                        break;
+        }
+
+        /* end of file without any bytes read */
+        if ((c == EOF) && (len == 0))
+                return -1;
+
+        /* trailing "\0" */
+        if (len >= *n) {
+                char *np = realloc( *lineptr, *n + 1 );
+                if (!np)
+                        return -1;
+                p = np + (p - *lineptr);
+                *lineptr = np;
+                *n += 1;
+        }
+        *p = '\0';
+
+        return len;
 }
 
 #endif /* !HAVE_GETDELIM */
