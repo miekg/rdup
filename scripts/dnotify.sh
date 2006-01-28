c6a17d6b86b73d5efe0ca41454397298f65df6c9
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-01-28 19:16:41 +0000

    various typos fixed
    git-svn-id: file:///svn/rdup/trunk@230 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

---
+++ scripts/dnotify.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# helper script to run rdup on a mounted usb backup disk
+# called by dnotify as
+# dnotify.sh <directory>
+
+# usb backup disklayout
+# /
+# /elektron
+#          /200601
+#          /200602
+# /host2
+# /host3
+
+# what file has changed?
+files=`ls -t $1`
+changed=`echo $files | awk '{ print $1 } '`
+
+# what is the latest backup of this host
+latest=`ls $1/$changed | sort -nr | head -1`
+
+# now if we are in the same month we do an incremental
+# otherwise we do a full dump
+
+d=`date +%Y%m`
+
+if [[ $d != $latest ]]; then
+        DUMP=full
+else
+        DUMP=incr
+fi
+
+echo $DUMP
+echo $changed
+echo $latest
+
+# rdup away
+
+
