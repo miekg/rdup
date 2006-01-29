e0cfe12167bbba81fa2681e81f6891196a1d05a0
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-01-29 18:01:53 +0000

    dnotify script which backups TO usb disk finished
    git-svn-id: file:///svn/rdup/trunk@234 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- scripts/dnotify.sh
+++ scripts/dnotify.sh
@@ -2,10 +2,15 @@
 
 # helper script to run rdup on a mounted usb backup disk
 # called by dnotify as
-# dnotify -e dnotify.sh {}
+# dnotify -e dnotify.sh {} dir [dir | ... ]
+# each dir is backed up
 
 # usb backup disklayout
 # /
+# /etc
+#     /elektron-timestamp
+#     /host2-timestamp
+#
 # /elektron
 #          /200601
 #          /200602
@@ -18,26 +23,45 @@
 #       /...
 
 # what directory has changed? And is our disk
+h=`hostname`
 files=`ls -t $1`
-changed=`echo $files | awk '{ print $1 } '`
+mountpoint=`echo $files | awk '{ print $1 } '`
+hostname=`ls $1/$mountpoint | sort -nr | head -1`
 
 # what is the latest backup of this host
-latest=`ls $1/$changed | sort -nr | head -1`
+# $changed/host -> backupdir
+# $changed/etc/host -> admin dir
+
+etc="$hostname/etc"
+backuphost="$hostname/$h"
+
+echo $etc
+echo $backuphost
 
 # now if we are in the same month we do an incremental
 # otherwise we do a full dump
 d=`date +%Y%m`
 
 if [[ $d != $latest ]]; then
-        DUMP=full
+        DUMP=null
+        echo rm -f "$etc/$h-timestamp"
 else
         DUMP=incr
 fi
 
+echo $d
 echo $DUMP
-echo $changed
-echo $latest
+backupdir=$backuphost/$d
 
-# rdup away
+if [[ ! -e "$backupdir" ]]; then
+        echo    mkdir -p "$backupdir"
+fi
+if [[ ! -e "$etc" ]]; then
+        echo    mkdir -p "$etc"
+fi
 
+# dirs in $@
+shift
 
+echo /usr/sbin/rdup -N "$etc/$h-timestamp" "$etc/$h-list" $@ 
+echo /usr/sbin/mirror.sh
