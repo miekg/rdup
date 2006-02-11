b6785b4a6c1e49a06c107f8c8da760693877c048
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-02-11 20:07:29 +0000

    hdup2 implemented in perl
    git-svn-id: file:///svn/rdup/trunk@258 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- hdup/hdup.pl
+++ hdup/hdup.pl
@@ -4,15 +4,39 @@
 # read the config file and use rdup to create the
 # tar archives.
 
+# we only allow
+# bzip2/gzip/none
+# date cannot be specified anymore
+# compression level gone
+
 use strict;
 use Config::IniFiles;
 use Unix::Syslog;
+use Getopt::Std;
 
-my $VERSION="rdup(hdup2) @RDUP_VERSION@";
+my $VERSION="\@PACKAGE_NAME\@(hdup2) \@PACKAGE_VERSION\@";
 
-my $hdupconf = "hdup.conf";
+my $TRUE = 1;
+my $FALSE = 0;
 my $cfg;
 my %rec;
+my $SCHEME;
+my $HOST;
+my %opt;
+
+# if yes/true/on -> TRUE
+# else no/false/off -> FALSE
+sub yesno {
+        if (!defined($_)) {
+                return $FALSE;
+        }
+        
+        if (/yes/i || /true/i || /on/i) {
+                return $TRUE;
+        } 
+        return $FALSE;
+}
+
 
 $rec{global} = {
         ARCHIVEDIR => '',       # backup directory
@@ -30,31 +54,80 @@ $rec{global} = {
         NOBACKUP => ''          # if there use .nobackup
         };
 
-# parse options
+# parse options/arguments
+getopts('c:', \%opt);
+
+my $CONFIG=$opt{c} || "/etc/hdup/hdup.conf";
+
+#if !defined then exit
+
+$SCHEME=$ARGV[0];   # null/monthly daily/one, weekly->monthly
+$HOST=$ARGV[1];     # the go'old host
+
+# get the scheme
+if ($SCHEME =~ /weekly/) {
+        warn "Weekly is not implemented, doing a null dump\n";
+        $SCHEME = "null";
+}
+if ($SCHEME =~ /monthly/) {
+        $SCHEME = "null";
+}
+if ($SCHEME =~ /daily/) {
+        $SCHEME = "one";
+}
+if ($SCHEME ne "null" && $SCHEME ne "one") {
+        die "Scheme $SCHEME is not known\n";
+}
 
 
+my (undef, undef, undef, $mday, $mon, $year, undef, undef, undef) =
+                localtime(time());
+my $DATE = ($year + 1900) . "-" . 
+                sprintf("%02d", ($mon + 1)). "-" . 
+                sprintf("%02d", $mday);
+my $BACKUPDIR = ($year + 1900) . sprintf("%02d", ($mon + 1));
 
-$cfg = Config::IniFiles->new(-file => "$hdupconf");
-
+$cfg = Config::IniFiles->new(-file => "$CONFIG") || die;
+if (! $cfg->SectionExists($HOST) ) {
+        die "No [$HOST] section in the configuration file: $CONFIG\n";
+}
 if (! $cfg->SectionExists('global') ) {
-        die "No [global] section in the configuration file: $hdupconf";
+        die "No [global] section in the configuration file: $CONFIG\n";
 }
 
 # read in the keywords
 foreach my $sec ($cfg->Sections) {
-        print "$sec\n";
         $rec{$sec}->{ARCHIVEDIR} = $cfg->val($sec, 'archive dir');
         $rec{$sec}->{COMPRESSION} = $cfg->val($sec, 'compression');
+        $rec{$sec}->{ONEFILESYSTEM} = yesno($cfg->val($sec, 'one filesystem'));
+        $rec{$sec}->{TAROPTION} = $cfg->val($sec, 'tar option');
+        $rec{$sec}->{TAR} = $cfg->val($sec, 'tar');
+
         # list structures
-        @{$rec{$sec}->{DIR}} = split /, */, $cfg->val($sec, 'dir');
+        @{$rec{$sec}->{DIR}} = split /, */, $cfg->val($sec, 'dir') if
+                                (defined($cfg->val($sec, 'dir')));
 }
-# inherit from [global] to children??
 
-print $rec{global}->{ARCHIVEDIR},"\n";
-print @{$rec{elektron}->{DIR}},"\n";
+# SCHEME is NULL
+my $TARFILE = "$HOST-$DATE.tar.gz";
+my $rdupcmd = "/home/miekg/svn/rdup/trunk/rdup -q $HOST-list " . join(' ', @{$rec{$HOST}->{DIR}});
+my $tarcmd  = "tar --create --gzip --no-recursion --file $TARFILE --files-from -";
 
+my $rdup = open(RDUP, "$rdupcmd |")  or die "Couldn't fork rdup: $!\n";
+my $tar  = open(TAR, "| $tarcmd")  or die "Couldn't fork tar: $!\n";
+while (<RDUP>) {
+        my ($mode, $uid, $gid, $psize, $fsize, $path) = split / /, $_;
 
-# run rdup, feed it's output to tar
-# put the archive some where, chown/chgrp it
-# encrypt it
-# ready
+        if (/^\+/) {
+                # only add '+' files
+               print TAR $path;
+        }
+}
+close(TAR)                          or die "Couldn't close tar: $!\n";
+close(RDUP)                         or die "Couldn't close rdup: $!\n";
+
+print $TARFILE,"\n";
+
+# encrypt
+# chown/chgrp
+# split?
--- rdup.c
+++ rdup.c
@@ -10,6 +10,7 @@ gboolean opt_null = FALSE;          /* delimit all in/output with \0  */
 gboolean opt_onefilesystem = FALSE; /* stay on one filesystem */
 gboolean opt_nobackup = TRUE;       /* don't ignore .nobackup files */
 gboolean opt_verbose = FALSE;       /* be more verbose */
+gboolean opt_quiet   = FALSE;       /* be more quiet */
 gboolean opt_contents = FALSE;      /* cat the file content to stdout */
 size_t opt_size  = 0;               /* only output files smaller then <size> */
 time_t opt_timestamp = 0;  /* timestamp file */
@@ -41,6 +42,7 @@ usage(FILE *f)
 	fprintf(f, "   \t\tif FILE does not exist, a full dump is performed\n");
 	fprintf(f, "   -s SIZE\tonly output files smaller then SIZE byes\n");
 	fprintf(f, "   -v\t\tbe more verbose\n");
+	fprintf(f, "   -q\t\tbe less verbose\n");
 	fprintf(f, "   -x\t\tstay in local file system\n");
 	fprintf(f, "   -0\t\tdelimit all output with NULLs\n");
 	fprintf(f, "\nReport bugs to <miek@miek.nl>\n");
@@ -190,7 +192,7 @@ main(int argc, char **argv)
 		}
 	}
 
-	while ((c = getopt (argc, argv, "chVnN:s:vx0")) != -1) {
+	while ((c = getopt (argc, argv, "chVnN:s:vqx0")) != -1) {
 		switch (c)
 		{
 			case 'h':
@@ -216,6 +218,9 @@ main(int argc, char **argv)
 				break;
 			case 'v':
 				opt_verbose = TRUE;
+				break;
+			case 'q':
+				opt_quiet = TRUE;
 				break;
 			case 'x':
 				opt_onefilesystem = TRUE;
@@ -285,11 +290,14 @@ main(int argc, char **argv)
 	g_tree_destroy(backup);
 	g_tree_destroy(remove);
 
-	fprintf(stderr, "** DIRECTORIES :");
+	if (!opt_quiet) 
+		fprintf(stderr, "** DIRECTORIES :");
 	for (i = 1; i < argc; i++) {
-		fprintf(stderr, " %s", argv[i]);
+		if (!opt_quiet) 
+			fprintf(stderr, " %s", argv[i]);
 	}
-	fprintf(stderr, "\n");
+	if (!opt_quiet)
+		fprintf(stderr, "\n");
 
 	exit(EXIT_SUCCESS);
 }
