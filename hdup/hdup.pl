6c1917ea278d8c154e6ac2c5003193011c442c0e
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-02-11 17:36:30 +0000

    addded beginning of hdup script
    git-svn-id: file:///svn/rdup/trunk@256 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

---
+++ hdup/hdup.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl -w
+
+# be a worthy replacement of the C version of hdup2
+# read the config file and use rdup to create the
+# tar archives.
+
+use strict;
+use Config::IniFiles;
+use Unix::Syslog;
+
+my $hdupconf = "hdup.conf";
+my $cfg;
+my %rec;
+
+$rec{global} = {
+        ARCHIVEDIR => '',       # backup directory
+        DATE => '',             # not impl. default to ISO
+        DIR => [],              # list of directories to backup
+        USER => '',             # owner of archive
+        GROUP => '',            # group of archive
+        PRERUN => '',           # pre run cmd
+        POSTRUN => '',          # post run cmd
+        FREE => '',             # how much free space 
+        COMPRESSION => '',      # compression program to use
+        ALGORITHM => '',        # encryption algorithm to use
+        KEY => '',              # encryption key file to use
+        ONEFILESYSTEM => '',    # yes/no one filesystem
+        NOBACKUP => ''          # if there use .nobackup
+        };
+
+# parse options
+
+
+
+$cfg = Config::IniFiles->new(-file => "$hdupconf");
+
+if (! $cfg->SectionExists('global') ) {
+        die "No [global] section in the configuration file: $hdupconf";
+}
+
+# read in the keywords
+foreach my $sec ($cfg->Sections) {
+        print "$sec\n";
+        $rec{$sec}->{ARCHIVEDIR} = $cfg->val($sec, 'archive dir');
+        $rec{$sec}->{COMPRESSION} = $cfg->val($sec, 'compression');
+        # list structures
+        @{$rec{$sec}->{DIR}} = split /, */, $cfg->val($sec, 'dir');
+}
+# inherit from [global] to children??
+
+print $rec{global}->{ARCHIVEDIR},"\n";
+print @{$rec{elektron}->{DIR}},"\n";
