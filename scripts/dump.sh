e1cb4c644b34eff50dad37cb9775e75e44c0b720
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-02-15 17:15:34 +0000

    scripts updated need to update the mirror script some more
    git-svn-id: file:///svn/rdup/trunk@268 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- scripts/dump.sh
+++ scripts/dump.sh
@@ -1,23 +1,70 @@
 #!/bin/sh
 
-# all relative to /etc/rdup
-# $1 = timestamp file
-# $2 = file list
-# $3 ... = dirs
+usage() {
+        echo "$0 [OPTIONS] NAME DIR [DIR ...]"
+        echo 
+        echo NAME - suffix for filelist and timestamp files
+        echo DIR  - directories to back up
+        echo
+        echo OPTIONS
+        echo "-b  backup directory. Default: /vol/backup/HOSTNAME"
+        echo "-e  filelist and timestamp are put in backup directory"
+        echo "-h  this help"
+}
 
-# so we need:
-# dump null timestamp.home filelist.home /home
+d=`date +%Y%m`
+etc=0
 
-type=$1  # null/incr
-name=$2
-# dirs in $@
-shift
-shift
-
-if [[ $type == "null" ]]; then
-        # kill the timestamp and inc list
-        rm -f /etc/rdup/ts-$name /etc/rdup/list-$name
+while getopts ":b:eh" o; do
+        case $o in
+                b) BACKUPDIR=$OPTARG;;
+                e) etc=1;;
+                h) usage && exit;;
+                \?) usage && exit;;
+        esac
+done
+shift $((OPTIND - 1))
+if [[ -z $1 ]]; then
+        echo "NAME is mandatory"
+        exit 1
+fi
+if [[ -z $BACKUPDIR ]]; then
+        BACKUPDIR="/vol/backup/$HOSTNAME"
 fi
 
-/usr/sbin/rdup -N /etc/rdup/ts-$name /etc/rdup/list-$name $dir | \
-/usr/sbin/excl.sh | /usr/sbin/mirror.sh
+if [[ $etc -eq 0 ]]; then
+        ETC="/etc/rdup"
+else
+        ETC="$BACKUPDIR"
+fi
+
+# create top-level backup dir
+
+NAME=$1  
+BACKUPDIR_DATE="$BACKUPDIR/$d"
+STAMP="$ETC/$HOSTNAME.$NAME.timestamp"
+LIST="$ETC/$HOSTNAME.$NAME.list"
+# DIRS in $@
+shift
+
+echo $BACKUPDIR
+echo $BACKUPDIR_DATE
+echo $STAMP
+echo $LIST
+echo $@
+
+echo mkdir -p "$BACKUPDIR"
+if [[ ! -d "$BACKUPDIR_DATE" ]]; then
+        # kill the timestamp and inc list
+        echo      mkdir -p "$BACKUPDIR_DATE"
+        echo      rm -f "$LIST"
+        echo      rm -f "$STAMP"
+fi
+
+if [[ -f /usr/bin/excl.sh ]]; then
+echo        /usr/sbin/rdup -N "$STAMP" "$LIST" $@ #|\
+echo        /usr/sbin/excl.sh  /usr/sbin/mirror.sh -b $BACKUPDIR
+else
+echo        /usr/sbin/rdup -N "$STAMP" "$LIST" $@  #|\
+echo        /usr/sbin/mirror.sh -b $BACKUPDIR
+fi
