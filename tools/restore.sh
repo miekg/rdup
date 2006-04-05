45d09554c9f626cd6c3672583213bb65828245b2
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-04-05 12:25:34 +0000

    add restore capability, based on .... rdup
    git-svn-id: file:///svn/rdup/trunk@467 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- tools/restore.sh
+++ tools/restore.sh
@@ -5,18 +5,46 @@
 # state (i.e. copy all files without the +extenstion)
 # -n DAYSAGO (restore to DAYSAGO's state)
 
+set -o nounset
 
-# go to the backup directory
-cd $1
+S_ISDIR=16384   # octal: 040000 (This seems to be portable...)
+S_ISLNK=40960   # octal: 0120000
+S_MMASK=4095    # octal: 00007777, mask to get permission
 
+#monthday=0
+monthday=11
+max=0
+prevfile=""
+name=""
+declare -a path
 
-for i in *; do 
-        if [[ $i =~ "\\+(.+)\\.(.+):(.+)$" ]]; then
-                days=${BASH_REMATCH[1]}
-                hour=${BASH_REMATCH[2]}
-                min=${BASH_REMATCH[3]}
-                continue;
+# one argument +n where n the monthday of the restore
+
+while read mode uid gid psize fsize path
+do
+        if [[ "$path" =~ "(.+)\\+(.+)\\.(.+):(.+)$" ]]; then
+                name=${BASH_REMATCH[1]}
+                days=$((10#${BASH_REMATCH[2]})) # force base 10
+                #hour=$((10#${BASH_REMATCH[3]}))
+                #min=$((10#${BASH_REMATCH[4]}))
+        else 
+                name="$path"
+                days=99
+                #hour=0
+                #min=0
         fi
 
-        echo "["$i"]"
+        if [[ "$prevfile" != "$name" ]]; then
+                # new name
+                echo $max "--" $prevfile
+                max=0
+        else 
+                if [[ ($days -le $monthday) && $days -gt $max ]]; then
+                        max=$days
+                fi
+        fi
+        
+        
+
+        prevfile="$name"
 done
