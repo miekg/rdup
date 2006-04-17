164325eb40c6731baf93c464b09bc2a4ab69e1ed
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-04-17 19:38:45 +0000

    re-implement -q
    git-svn-id: file:///svn/rdup/trunk@550 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- gfunc.c
+++ gfunc.c
@@ -10,6 +10,7 @@
 extern gboolean opt_null;
 extern gboolean opt_removed;
 extern gboolean opt_modified;
+extern gboolean opt_slash;
 extern gint opt_verbose;
 extern char *opt_format;
 extern char qstr[];
@@ -35,6 +36,36 @@ signal_abort(int signal)
 			break;
 	}
 	exit(EXIT_FAILURE);
+}
+
+/*
+ * quote backslashes - so that shell programs can deal with
+ * the output. This uses static memory for speed.
+ */
+static char *
+quote(char *f)
+{
+	char *j;
+	size_t i;
+
+	if (!opt_slash) {
+		return f;
+	}
+
+	for(i = 0, j = f; *j; j++) {
+		if (*j == '\\') {
+			qstr[i++] = '\\';
+
+		}
+		qstr[i++] = *j;
+		if (i > (BUFSIZE - 2)) {
+			fprintf(stderr, "** %s: Name to long, chopping\n", PROGNAME);
+			qstr[BUFSIZE] = '\0';
+			return qstr;
+		}
+	}
+	qstr[i] = '\0';
+	return qstr;
 }
 
 /*
@@ -145,10 +176,10 @@ entry_print_data(FILE *out, char n, struct entry *e)
 {
 	switch (n) {
 		case 'n': 
-			fputs(e->f_name, out);		
+			fputs(quote(e->f_name), out);		
 			break;
 		case 'l': 
-			fprintf(out, "%zd", e->f_name_size);	
+			fprintf(out, "%zd", e->f_name_size + e->f_slash);	
 			break;
 		case 'u': 
 			fprintf(out, "%d", e->f_uid);		
