96ad59d9e24f5df6b12360c9a516a2fd97d56fc8
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-05-10 13:05:53 +0000

    unveil the poster
    git-svn-id: file:///svn/rdup/trunk@747 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- makedist.sh
+++ makedist.sh
@@ -118,7 +118,7 @@ autoconf || error_cleanup "Autoconf failed."
 
 rm -r autom4te* || error_cleanup "Failed to remove autoconf cache directory."
 rm -rf patches  || error_cleanup "Failed to remove patches directory."
-#rm -rf poster   || error_cleanup "Failed to remove poster directory."
+rm -rf poster/icons   || error_cleanup "Failed to remove poster/icons directory."
 rm -rf feedback   || error_cleanup "Failed to remove feedback directory."
 
 find . -name .c-mode-rc.el -exec rm {} \;
