6c38aa3d08476e5f553417ea156b2a2edc81d711
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2006-12-30 22:57:41 +0000

    fix more website stuff
    git-svn-id: file:///svn/rdup/trunk@1154 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

---
+++ php/rdup-index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta name="description" content="rdup configuration"/>
+<meta name="keywords" content="miek, rdup"/>
+<meta name="author" content="Miek Gieben"/>
+  <style type="text/css" media="screen">
+  <!--
+    @import url("/miek.css");
+  -->
+  </style>
+  <title>rdup - infopage</title>
+</head>
+<body id="mainend">
+<div id="wrap">
+<div id="innerWrap">
+<!-- header -->
+<div id="header">
+<div id="headerLeft">
+</div>
+<div>
+</div>
+</div><!-- end header -->
+
+<!-- MAIN NAVIGATION -->
+<div id="mainNavOuter">
+<div id="mainNav">
+<div id="mainNavInner">
+<ul>
+<!-- mainLast is highlighted -->
+    <li id="mainLast"><a href="http://www.miek.nl/">Info</a></li>
+    <li id="m1"><a href="http://www.miek.nl/projects/">Backups</a></li>
+    <li id="m2"><a href="http://www.miek.nl/projects/">Configuration</a></li>
+    <li id="m3"><a href="http://www.miek.nl/linux/">System</a></li>
+    <li id="m4"><a href="http://www.miek.nl/projects/rdup/">Homepage rdup</a></li>
+</ul>
+</div><!-- end mainNavInner -->
+</div><!-- end mainNav -->
+</div><!-- end mainNavOuter -->
+
+<div id="secNavOuter">
+<div id="secNav">
+<div id="secNavInner">
+
+</div><!-- end secNavInner -->
+</div><!-- end secNav -->
+</div><!-- end secNavOuter -->
+<span id="login-info">
+2005 - 2007
+ &copy; Miek Gieben
+</span>
+<div style="clear: both;"></div>
+
+<!-- MAIN CONTENT AREA -->
+<div id="contentWrap">
+
+  <!-- CONTENT COLUMN -->
+  <div id="content1Col">
+  <div id="contentIndent">
+    <h1>Backups</h1>
+ <div class="content-nav">
+</div>
+
+
+<div id="content">
+
+<h2>rdup 0.3.5</h2>
+<div id="info">Fri Dec 27 19:53:59 CET 2006</div>
+An exclude (-E) option was added, rdup-restore is
+refactored.  See the 
+<a href="projects/rdup/index.html">project page</a> for details. 
+<p/>
+<a href="projects/rdup/rdup.tar.bz2">download</a>
+
+
+<h2>RAID on Linux, booting from USB</h2>
+<div id="info">Tue Sep  5 09:09:10 CEST 2006</div>
+I'm building a RAID server which boots from a USB stick.
+This server will be low maintenance and high capacity.
+<p/>
+Read <a href="/linux/usb_raid.html">how I did it</a>.
+
+<h2>New LaTeX class</h2>
+<div id="info">Fri jun 23 08:38:41 CEST 2006</div>
+I've created a new LaTeX layout, this time based on
+the excellent <tt>memoir</tt> class. Check it
+<a href="linux/blocks.html">out</a>.
+
+</div> <!-- content -->
+
+
+</div><!-- contentIndent -->
+</div><!-- content1Col -->
+</div><!-- contentWrap -->
+<p id="footer"> Copyright &copy; 2005 - 2007 Miek Gieben<br/>
+  <a href="http://www.miek.nl/projects/rdup">Homepage rdup</a> :
+  <a href="http://www.miek.nl/about/about.html">Contact author</a>
+</p>
+</div> <!-- end innerWrap -->
+</div> <!-- end wrap --></body></html>
