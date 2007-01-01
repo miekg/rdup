4d581b05af01c654fcf4a9edb3636426bc57936b
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2007-01-01 10:36:46 +0000

    progress stuff
    git-svn-id: file:///svn/rdup/trunk@1165 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- php/index.php
+++ php/index.php
@@ -6,6 +6,8 @@ $w = new web($L, "lang.txt");
 $w->header("infopage");
 ?>
 
+<script src="/php/progressbar.js" type="text/javascript" language="javascript1.2"></script>
+
 <h2>rdup 0.3.5</h2>
 <div id="info">Fri Dec 27 19:53:59 CET 2006</div>
 An exclude (-E) option was added, rdup-restore is
@@ -13,6 +15,28 @@ refactored.  See the
 <a href="projects/rdup/index.html">project page</a> for details. 
 <p/>
 <a href="projects/rdup/rdup.tar.bz2">download</a>
+
+    <script type="text/javascript" language="javascript1.2"><!--
+        var myProgBar = new progressBar(
+            1,         //border thickness
+            '#000000', //border colour
+            '#a5f3b1', //background colour
+            '#043db2', //bar colour
+            400,       //width of bar (excluding border)
+            20,        //height of bar (excluding border)
+            1          //direction of progress: 1 = right, 2 = down, 3 = left, 4 = up
+        );
+    //--></script>
+
+<script type="text/javascript" language="javascript1.2"><!--
+
+myProgBar.setBar(0.5);       //set the progress bar to indicate a progress of 50%
+myProgBar.setBar(0.1,true);  //add 10% to the progress bar's progress
+myProgBar.setBar(0.1,false); //subtract 10% from the progress bar's progress
+myProgBar.setCol('#ff0000'); //change the colour of the progress bar
+alert( 'Current progress is ' + myProgBar.amt ) // read current progress
+
+    //--></script>
 
 
 <h2>RAID on Linux, booting from USB</h2>
