4c3242e50e1976b5a83c2b0b9334bebbe1c2543b
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2008-03-09 20:38:27 +0000

    removing old cruft
    git-svn-id: file:///svn/rdup/trunk@1405 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- php/menu.js
+++
@@ -1,5 +0,0 @@
-function toggleMenu(objID) {
-        if (!document.getElementById) return;
-        var ob = document.getElementById(objID).style;
-        ob.display = (ob.display == 'block')?'none': 'block';
-}
