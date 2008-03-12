b66219628cd839db7f5488995cc31030e7ee33d5
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2008-03-12 07:54:22 +0000

    add my own realpath function
    git-svn-id: file:///svn/rdup/trunk@1409 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

---
+++ rlpath.c
@@ -0,0 +1,13 @@
+#include "rdup.h"
+
+/* For each pathname remove references to /../ and /./
+ * and remove extranous //
+ * Return the sanitized string
+ * See man realpath(3)
+ */
+
+char * rlpath(char *path) {
+
+
+    return NULL;
+}
