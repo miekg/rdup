f4aff6eb12d8e0071baa5df0fef8a9cca2b418d0
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2008-05-18 08:59:40 +0000

    fix the usage and fiddle a bit with the man pages
    git-svn-id: file:///svn/rdup/trunk@1495 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

--- usage.c
+++ usage.c
@@ -8,7 +8,7 @@
 void
 usage(FILE *f)
 {
-        fprintf(f, _("USAGE: %s [OPTION...] FILELIST DIR|FILE...\n"), PROGNAME);
+        fprintf(f, _("USAGE: %s [OPTION]... FILELIST DIR|FILE...\n"), PROGNAME);
 	fputs( _("\
 Generate a full or incremental file list. This list can be\n\
 implement a (incremental) backup scheme.\n\
@@ -21,18 +21,18 @@ implement a (incremental) backup scheme.\n\
         -N FILE\tuse the timestamp of FILE for incremental dumps\n\
         \t\tif FILE does not exist, a full dump is performed\n\
         -F FORMAT\tuse specified format string\n\
-        \t\tdefaults to: \"%%p%%T %%b %%u %%g %%l %%s %%n\\n\"\n\
+        \t\tdefaults to: \"%p%T %b %u %g %l %s %n\\n\"\n\
 	-E FILE\tuse FILE as an exclude list\n\
         -0\t\tdelimit internal filelist with NULLs\n\
         -V\t\tprint version\n"), f);
 #ifdef HAVE_ATTR_XATTR_H
-	fputs( _("-a\t\tread the extended attributes: r_uid, r_gid\n"), f);
+	fputs( _("	-a\t\tread the extended attributes: r_uid, r_gid\n"), f);
 #endif /* HAVE_ATTR_XATTR_H */
 #ifdef HAVE_ATTROPEN
-        fputs( _("-a\t\tread the extended attributes: r_uid, r_gid\n"), f);
+        fputs( _("	-a\t\tread the extended attributes: r_uid, r_gid\n"), f);
 #endif /* HAVE_ATTROPEN */
 	fputs( _("\
-        -c\t\tcat the contents (FORMAT=\"%%p%%T %%b %%u %%g %%l %%s %%n%%C\")\n\
+        -c\t\tcat the contents (FORMAT=\"%p%T %b %u %g %l %s %n%C\")\n\
         -h\t\tthis help\n\
         -m\t\tonly print new/modified files (unsets -r)\n\
         -l\t\tdon't check for file size changes\n\
