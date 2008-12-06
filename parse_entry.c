5b5806efd0a42e5dac3915c6d7d60df1dc9354e7
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2008-12-06 22:45:34 +0000

    add parse_entry for parsing an rdup entry
    git-svn-id: file:///svn/rdup/trunk@1689 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

---
+++ parse_entry.c
@@ -0,0 +1,17 @@
+/* 
+ * Copyright (c) 2009 Miek Gieben
+ * parse_entry.c
+ * parse an standard rdup entry and return a
+ * struct r_entry
+ */
+
+#include "rdup-tr.h"
+
+/* buf is NULL delimited */
+struct r_entry *
+parse_in(char *buf) 
+{
+	buf = buf;
+
+	return NULL;
+}
