347a75953c748529c9a10e3e98e4023c946385d4
miekg <miekg@0f4cb41f-7f08-0410-98f9-8ec83d93c06f>
2008-12-26 16:23:14 +0000

    add a link script back in, but install it in /usr/lib/rdup
    git-svn-id: file:///svn/rdup/trunk@1750 0f4cb41f-7f08-0410-98f9-8ec83d93c06f

---
+++ sh/rdup-ln.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# hardlink a previous backup directory
+# to a new directory
+
+# SYNOPSIS: rdup-ln.sh [-l N] BACKUPDIR
+# -l N: look back N days (default 8)
+# -n  : dry run: don't touch the filesystem
+# BACKUPDIR: top directory of the backups
+#
+# By default a new directory
+# BACKUPDIR/YYYYMM/DD wil be created
+
+# Three returns code
+# 0: BACKUPDIR/YYYYMM/DD is created (now make a inc dump)
+# 1: BACKUPDIR/YYYYMM/DD is created (now make a full dump)
+# 2: an error occured
+
+DATESTR='+%Y%m/%d'
+TODAY=$(date $DATESTR)
+TOPDIR=$1
+LOOKBACK=8
+
+if [[ -z $TOPDIR ]]; then
+    exit 2
+fi
+
+if [[ -d $TOPDIR/$TODAY ]]; then
+    exit 0
+else
+    if ! mkdir -p $TOPDIR/$TODAY; then
+    exit 2
+    fi
+fi
+
+let i=1
+while [[ $i -le $LOOKBACK ]]; do
+	D=$(date $DATESTR --date "$i days ago")
+	echo $D >&2
+	if [[ -d $TOPDIR/$D ]]; then
+	    echo "Hardlinking: \`$TOPDIR/$D'" >&2
+	    if ! cp -plr $TOPDIR/$D $TOPDIR/$TODAY; then
+		exit 2
+	    fi
+	    return 0
+	fi
+        let i=i+1
+done
+exit 1
