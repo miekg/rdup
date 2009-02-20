1f97c4372620d8640b60de9bf357c09809f6e2be
Miek Gieben <miekg@atoom.net>
2009-02-20 22:08:04 +0100

    add dir.c for the dir write stuff

---
+++ dir.c
@@ -0,0 +1,20 @@
+/* 
+ * Copyright (c) 2009 Miek Gieben
+ * See LICENSE for the license
+ * make a dir writable and later remove that
+ * right again
+ */
+
+#include "rdup-up.h"
+
+struct stat *
+dir_write(gchar *p)
+{
+
+}
+
+void
+dir_restore(gchar *p, struct stat *s)
+{
+
+}
