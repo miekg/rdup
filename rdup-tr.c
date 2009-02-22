9610920964cbe8c9b210597f5ef037f62aa97c8d
Miek Gieben <miekg@elektron2.atoom.net>
2009-02-22 22:04:54 +0100

    Read decryption key and set the correct vars

--- rdup-tr.c
+++ rdup-tr.c
@@ -422,9 +422,9 @@ main(int argc, char **argv)
 				aes_ctx = crypt_init(opt_crypt_key, strlen(opt_crypt_key), TRUE);
 				break;
 			case 'Y':
-				if (! (opt_crypt_key = crypt_key(optarg)))
+				if (! (opt_decrypt_key = crypt_key(optarg)))
 					exit(EXIT_FAILURE);
-				aes_ctx = crypt_init(opt_crypt_key, strlen(opt_crypt_key), TRUE);
+				aes_ctx = crypt_init(opt_decrypt_key, strlen(opt_decrypt_key), FALSE);
 				break;
 			case 'h':
 				usage_tr(stdout);
