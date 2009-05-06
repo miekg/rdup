8cdf8288c34217c9f80deb30f7505e6c287c599d
Miek Gieben <miek@miek.nl>
2009-05-06 17:00:13 +0200

    refuse to run rdup-tr -X when encryption is not compiled in

--- rdup-tr.c
+++ rdup-tr.c
@@ -442,6 +442,9 @@ main(int argc, char **argv)
 				if (! (opt_crypt_key = crypt_key(optarg)))
 					exit(EXIT_FAILURE);
 				aes_ctx = crypt_init(opt_crypt_key, strlen(opt_crypt_key), TRUE);
+#else
+				msg(_("Compiled without encryption, can not encrypt"));
+				exit(EXIT_FAILURE);
 #endif
 				break;
 			case 'Y':
@@ -453,6 +456,9 @@ main(int argc, char **argv)
 				if (! (opt_decrypt_key = crypt_key(optarg)))
 					exit(EXIT_FAILURE);
 				aes_ctx = crypt_init(opt_decrypt_key, strlen(opt_decrypt_key), FALSE);
+#else
+				msg(_("Compiled without encryption, can not decrypt"));
+				exit(EXIT_FAILURE);
 #endif
 				break;
 			case 'h':
