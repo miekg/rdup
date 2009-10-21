97603b9b79f74d811ac1ddf0d8d70026cd11f022
Miek Gieben <miek@miek.nl>
2009-10-21 22:24:32 +0200

    SHA1 in openssl and more
    Sha1 impl now in openssl. And uncomment stream stuff

--- entry.c
+++ entry.c
@@ -216,9 +216,6 @@ parse_entry(char *buf, size_t l, struct stat *s)
 	return e;
 }
 
-/* NEED TO FIX THIS, the the gfunc equavalent */
-
-#if 0
 /* ALmost the same of entry_print_data in gfunc.c, but
  * not quite as we don't don't use FILE* structs here
  * for instance. TODO: integrate the two functions?
@@ -284,4 +281,3 @@ rdup_write_data(__attribute__((unused)) struct rdup *e, char *buf, size_t len) {
 		return -1;
 	return 0;
 }
-#endif
--- sha1.c
+++ sha1.c
@@ -1,28 +1,25 @@
-/* sha1.c - copied from
-   /usr/share/doc/libnettle-dev/examples/sha-example.c
-   */
-
 #include "rdup.h"
 #include <openssl/sha.h>
 
 int
 sha1_stream(FILE *f, unsigned char *digest)
 {
-	struct sha1_ctx ctx;
-	uint8_t buffer[BUFSIZE];
-	/*  uint8_t digest[SHA1_DIGEST_SIZE]; */
+	SHA_CTX *c = NULL;
+	guint buffer[BUFSIZE];
+/*	SHA_DIGEST_LENGTH  */
 
-	sha1_init(&ctx);
+	SHA1_Init(c);
 	for (;;)
 	{
 		guint done = fread(buffer, 1, sizeof(buffer), f);
-		sha1_update(&ctx, done, buffer);
+		SHA1_Update(c, buffer, done);
+/*		sha1_update(&ctx, done, buffer);*/
 		if (done < sizeof(buffer))
 			break;
 	}
 	if (ferror(f))
 		return -1;
 
-	sha1_digest(&ctx, SHA1_DIGEST_SIZE, digest);
+	SHA1_Final(digest, c);
 	return 0;  
 }
