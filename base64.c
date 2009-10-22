1253b807a0a4996e096c5fe8a91b68063474ae9b
Miek Gieben <miek@miek.nl>
2009-10-22 19:28:14 +0200

    whitespace changes

--- base64.c
+++ base64.c
@@ -29,19 +29,10 @@ static unsigned char decode(char c);
 
 /**
  *  Implementation of base64 encoding/decoding. 
- *
  *  @author Jan-Henrik Haukeland, <hauk@tildeslash.com>
- *
  *  @version \$Id: base64.c,v 1.19 2007/07/25 12:54:31 hauk Exp $
- *
- *  @file
+ *  
  */
-
-
-
-/* ------------------------------------------------------------------ Public */
-
-
 
 /**
  * Base64 encode and return size data in 'src'. The caller must free the
@@ -50,8 +41,8 @@ static unsigned char decode(char c);
  * @param src The data to be base64 encode
  * @return encoded string otherwise NULL
  */
-char *encode_base64(int size, unsigned char *src) {
-
+char *encode_base64(int size, unsigned char *src) 
+{
   int i;
   char *out, *p;
 
@@ -61,9 +52,8 @@ char *encode_base64(int size, unsigned char *src) {
   if(!size)
     size= strlen((char *)src);
     
-  out= g_malloc0(size*4/3+4);
-
-  p= out;
+  out = g_malloc0(size*4/3+4);
+  p = out;
     
   for(i=0; i<size; i+=3) {
       
@@ -96,11 +86,8 @@ char *encode_base64(int size, unsigned char *src) {
     } else {
       *p++= '=';
     }
-
   }
-
   return out;
-
 }
 
 
@@ -113,47 +100,35 @@ char *encode_base64(int size, unsigned char *src) {
  * @return TRUE (the length of the decoded string) if decode
  * succeeded otherwise FALSE.
  */
-int decode_base64(unsigned char *dest, const char *src) {
-
+int decode_base64(unsigned char *dest, const char *src) 
+{
   if(src && *src) {
   
     unsigned char *p= dest;
     int k, l= strlen(src)+1;
     unsigned char *buf= g_malloc0(l);
-
     
     /* Ignore non base64 chars as per the POSIX standard */
     for(k=0, l=0; src[k]; k++) {
-      
       if(is_base64(src[k])) {
-	
 	buf[l++]= src[k];
-	
       }
-      
     } 
     
     for(k=0; k<l; k+=4) {
-      
       char c1='A', c2='A', c3='A', c4='A';
       unsigned char b1=0, b2=0, b3=0, b4=0;
       
       c1= buf[k];
-      
       if(k+1<l) {
-	
 	c2= buf[k+1];
-	
       }
       
       if(k+2<l) {
-	
 	c3= buf[k+2];
-	
       }
       
       if(k+3<l) {
-	
 	c4= buf[k+3];
 	
       }
@@ -166,38 +141,24 @@ int decode_base64(unsigned char *dest, const char *src) {
       *p++=((b1<<2)|(b2>>4) );
       
       if(c3 != '=') {
-	
 	*p++=(((b2&0xf)<<4)|(b3>>2) );
-	
       }
-      
       if(c4 != '=') {
-	
 	*p++=(((b3&0x3)<<6)|b4 );
-	
       }
-      
     }
     
     g_free(buf);
-    
     return(p-dest);
-
   }
-
   return FALSE;
-  
 }
-
-
-/* ----------------------------------------------------------------- Private */
-
 
 /**
  * Base64 encode one byte
  */
-static char encode(unsigned char u) {
-
+static char encode(unsigned char u) 
+{
   if(u < 26)  return 'A'+u;
   if(u < 52)  return 'a'+(u-26);
   if(u < 62)  return '0'+(u-52);
@@ -205,38 +166,29 @@ static char encode(unsigned char u) {
   
   /* return '/'; */
   return '_';
-
 }
-
 
 /**
  * Decode a base64 character
  */
-static unsigned char decode(char c) {
-  
+static unsigned char decode(char c) 
+{
   if(c >= 'A' && c <= 'Z') return(c - 'A');
   if(c >= 'a' && c <= 'z') return(c - 'a' + 26);
   if(c >= '0' && c <= '9') return(c - '0' + 52);
   if(c == '-')             return 62;  /* was '+' */
-  
   return 63;
-  
 }
-
 
 /**
  * Return TRUE if 'c' is a valid base64 character, otherwise FALSE
  */
-static int is_base64(char c) {
-
+static int is_base64(char c) 
+{
   if((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
      (c >= '0' && c <= '9') || (c == '-')             ||
      (c == '_')             || (c == '=')) {
-    
     return TRUE;
-    
   }
-  
   return FALSE;
-
 }
