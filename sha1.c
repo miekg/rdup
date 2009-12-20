3a8a35db8cfe1aa7fc857b945cec95bc89c45379
Miek Gieben <miek@miek.nl>
2009-12-20 10:50:08 +0100

    Kill OpenSSL and go with trusted libnettle

--- sha1.c
+++ sha1.c
@@ -1,23 +1,28 @@
+/* sha1.c - copied from
+   /usr/share/doc/libnettle-dev/examples/sha-example.c
+   */
+
 #include "rdup.h"
-#include <openssl/sha.h>
+#include <nettle/sha.h>
 
 int
 sha1_stream(FILE *f, unsigned char *digest)
 {
-	guint buffer[BUFSIZE], done;
-	SHA_CTX *c = g_malloc(sizeof(SHA_CTX));
+	struct sha1_ctx ctx;
+	uint8_t buffer[BUFSIZE];
+	/*  uint8_t digest[SHA1_DIGEST_SIZE]; */
 
-	SHA1_Init(c);
+	sha1_init(&ctx);
 	for (;;)
 	{
-		done = fread(buffer, 1, sizeof(buffer), f);
-		SHA1_Update(c, buffer, done);
+		guint done = fread(buffer, 1, sizeof(buffer), f);
+		sha1_update(&ctx, done, buffer);
 		if (done < sizeof(buffer))
 			break;
 	}
 	if (ferror(f))
 		return -1;
 
-	SHA1_Final(digest, c);
+	sha1_digest(&ctx, SHA1_DIGEST_SIZE, digest);
 	return 0;  
 }
