cff24b653517260b310d89b7910f01941e4d66d2
Miek Gieben <miek@miek.nl>
2010-04-13 10:38:07 +0200

    doc update

--- contrib/convert.pl
+++ contrib/convert.pl
@@ -1,4 +1,7 @@
 #!/usr/bin/perl -wn
+# convert a pre 1.1.x internal rdup filelist to rdup 1.1.x format
+# Author: Miek Gieben (miek@miek.nl)
+# License: GPLv3
 split;
 if ($_[0] eq "41471") { $l[3] = "l" } # symlink
 if ($_[3] eq "*")     { $l[3] = "-" }   
