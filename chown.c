755f750a7091c9fe697e83dda3f964fda37c760a
Miek Gieben <miek@miek.nl>
2012-05-24 22:08:44 +0200

    More stuff for parsing the files

--- chown.c
+++ chown.c
@@ -7,3 +7,27 @@
 
 /* signal.c */
 void got_sig(int signal);
+
+/* write a chown helper file */
+void
+chown_write(gchar *dir, gchar *base, uid_t u, gchar *user, gid_t g, gchar *group)
+{
+        FILE *f;
+        gchar *path = g_strdup_printf("%s/%s%s", dir, USRGRPINFO, base);
+        if ( ! ( f = fopen(path, "r"))) {
+                /* no file found or failure to open */
+                g_free(path);
+                return;
+        }
+        g_free(path);
+        fprintf(f, "%s:%d/%s:%d", user, u, group, g);
+        fclose(f);
+}
+
+/* parse the chown help file */
+chown_pack
+chown_parse(gchar *dir, gchar *base)
+{
+        dir = dir; base = base;
+        return NULL;
+}
