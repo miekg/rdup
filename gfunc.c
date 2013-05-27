460cecaa96c16635658a7a1591f3d28645f8f709
Miek Gieben <miek@miek.nl>
2013-05-27 15:11:25 +0100

    Print off_t with %jd not %zd
    This is to print size > 2GB correctly.

--- gfunc.c
+++ gfunc.c
@@ -309,11 +309,11 @@ static void entry_print_data(FILE * out, char n, struct rdup *e)
 		}
 		/* links */
 		if (S_ISLNK(e->f_mode) || e->f_lnk == 1) {
-			fprintf(out, "%zd", e->f_size);
+			fprintf(out, "%jd", e->f_size);
 			break;
 		}
 
-		fprintf(out, "%zd", e->f_size);
+		fprintf(out, "%jd", e->f_size);
 		break;
 	case 'H':		/* sha1 hash */
 		if (S_ISREG(e->f_mode)) {
